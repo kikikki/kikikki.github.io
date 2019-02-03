a65b599a32e7a6db0cbd83e58ac82096e8bc8bfe
kikikki <joolsdog93@gmail.com>
2019-02-03 12:51:34 +0400

    тестовое задание

--- test/js/script.js
+++ test/js/script.js
@@ -7,9 +7,18 @@ $(document).ready(function(){
         $('#' + this.id + '_face').css('display','flex');
         $('.faces').children(".person_item").children("span").css('display','none');
         $('#' + this.id + '_face').children('.person_item').first().children("span").css('display','block');
+        $('.person_body').css('display','none');
+        $('#pbody_' + this.id).css('display','block');
+        $('.person_face').css('display','none');
+        $('#pface_' + this.id + '_one').css('display','block');
+
     });
     $('.faces > .person_item').on("click", function(e){
         $(this).parent().children(".person_item").children("span").css('display','none');
         $(this).children("span").css('display','block');
+        $('.person_face').css('display','none');
+        $('#pface_' + this.id).css('display','block');
+        
     });
+
 });
